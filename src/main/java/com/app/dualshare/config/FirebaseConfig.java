4f5f2dbe06cae0770c37f2ae53ae05786889c072
Emir Guanactolay <emir201xd@gmail.com>
2026-07-31 13:05:41 -0300

    updates

--- src/main/java/com/app/dualshare/config/FirebaseConfig.java
+++ src/main/java/com/app/dualshare/config/FirebaseConfig.java
@@ -3,36 +3,51 @@ package com.app.dualshare.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import jakarta.annotation.PostConstruct;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${FIREBASE_CONFIG}")
+    private String firebaseConfig;
 
     @PostConstruct
     public void initializeFirebase() {
         try {
-            InputStream serviceAccount = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("firebase-service-account.json");
 
-            if (serviceAccount == null) {
-                throw new IOException("No se encontro el archivo firebase-service-account.json en resources");
-            }
+            InputStream serviceAccount =
+                    new ByteArrayInputStream(
+                            firebaseConfig.getBytes(StandardCharsets.UTF_8)
+                    );
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+            FirebaseOptions options =
+                    FirebaseOptions.builder()
+                            .setCredentials(
+                                    GoogleCredentials.fromStream(serviceAccount)
+                            )
+                            .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-                System.out.println("====== [DUALSHARE] Firebase Admin SDK inicializado con éxito ======");
-            }
 
-        } catch (IOException e) {
-            System.err.println("Error al inicializar Firebase Admin SDK: " + e.getMessage());
+                FirebaseApp.initializeApp(options);
+
+                System.out.println(
+                        "====== [DUALSHARE] Firebase Admin SDK inicializado con éxito ======"
+                );
+
+            }
+        } catch (
+                IOException e) {
+            System.err.println(
+                    "Error al inicializar Firebase Admin SDK: "
+                            + e.getMessage()
+            );
         }
     }
 }
